46063b312191a25f182ba67c0572d9d5f0287d85
Lmilne <lmilne360@gmail.com>
2017-08-05 13:45:23 -0400

    Generated basic components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ImageDetailComponent } from './image-detail/image-detail.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { UploadComponent } from './upload/upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    ImageDetailComponent,
+    NavbarComponent,
+    LoginComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule
